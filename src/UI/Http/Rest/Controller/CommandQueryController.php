2227159b97a3ec911ba419490947d46d0e813c55
Abelardo <diox76@gmail.com>
2020-07-03 12:46:27 +0200

    Added support to retrieve logged user information from any controller

--- src/UI/Http/Rest/Controller/CommandQueryController.php
+++ src/UI/Http/Rest/Controller/CommandQueryController.php
@@ -7,22 +7,29 @@ namespace App\UI\Http\Rest\Controller;
 use App\Infrastructure\Shared\Bus\Command\CommandBus;
 use App\Infrastructure\Shared\Bus\Command\CommandInterface;
 use App\Infrastructure\Shared\Bus\Query\QueryBus;
+use App\Infrastructure\User\Auth\Session;
 use App\UI\Http\Rest\Response\JsonApiFormatter;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 abstract class CommandQueryController extends QueryController
 {
     private CommandBus $commandBus;
+    /**
+     * @var Session
+     */
+    private Session $session;
 
     public function __construct(
-        CommandBus $commandBus,
-        QueryBus $queryBus,
-        JsonApiFormatter $formatter,
-        UrlGeneratorInterface $router
+        CommandBus $commandBus
+        , QueryBus $queryBus
+        , JsonApiFormatter $formatter
+        , UrlGeneratorInterface $router
+        , Session $session
     ) {
         parent::__construct($queryBus, $formatter, $router);
 
         $this->commandBus = $commandBus;
+        $this->session = $session;
     }
 
     /**
@@ -31,5 +38,14 @@ abstract class CommandQueryController extends QueryController
     protected function exec(CommandInterface $command): void
     {
         $this->commandBus->handle($command);
+    }
+
+    /**
+     * @returns [ 'uuid': string, 'username': string, 'roles': array<string>]
+     * @throws App\Domain\User\Exception\InvalidCredentialsException
+     */
+    protected function userFromSession(): array
+    {
+        return $this->session->get();
     }
 }
